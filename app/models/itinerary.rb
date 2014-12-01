87eecaa740e82a99bebb7be1bd56518469c90835
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-30 19:54:48 -0500

    add itinerary validations

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,8 +1,10 @@
 class Itinerary < ActiveRecord::Base
   validates :start_date, :end_date, presence:true
+  validates_numericality_of :end_date, greater_than_or_equal_to: :start_date
 
   has_many :travel_dates
   has_many :weathers, through: :travel_dates
   has_one :location
   belongs_to :trip
+
 end
