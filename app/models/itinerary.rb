27e2ea0fc4f8e6d1c62064651e2f2c6035a1d984
Viktor Nikitenko <vnikiten01@gmail.com>
2014-11-30 22:36:30 -0500

    Fixed validation of dates in itinerary

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -1,10 +1,14 @@
 class Itinerary < ActiveRecord::Base
   validates :start_date, :end_date, presence:true
-  validates_numericality_of :end_date, greater_than_or_equal_to: :start_date
+  validate :start_date_before_or_equal_to_end_date
 
   has_many :travel_dates
   has_many :weathers, through: :travel_dates
   has_one :location
   belongs_to :trip
 
+ 
+ 	def start_date_before_or_equal_to_end_date
+ 		errors.add(:end_date, "End Date must be same or more than start_date") if end_date<start_date
+ 	end
 end
